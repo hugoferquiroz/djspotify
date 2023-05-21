f7816bb2dcc1c81ff7d8bce6819f186c5eae6384
Hugo Fernandez <32678317+hugoferquiroz@users.noreply.github.com>
2023-05-21 10:15:19 -0500

    set cookiecutter files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This virtual DJ creates playlists based on your preferences as well as those of your friends. It utilizes artificial intelligence and the Spotify API to curate personalized playlists.',
+    author='Cesar Quezada, Hugo Fernandez, Jack Cruz and Walter Peña',
+    license='MIT',
+)
